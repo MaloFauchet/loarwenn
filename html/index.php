4a47c9f17bc9827398cb74626f920fa357edbb0d
FAUCHET Malo <malofauchetpro@gmail.com>
2025-05-15 17:47:58 +0200

    started front footer

--- html/index.php
+++ html/index.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<?php
-
-echo "<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PACT</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
 <body>
-
-<h1>The server works !</h1>
-
+    <?php // Include the footer
+    require_once '../phpTemplates/footerFront.php';
+    ?>
 </body>
 </html>
-";
-
-?>
