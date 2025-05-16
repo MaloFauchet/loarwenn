a1383ae19d8b477cd9700d0922f352551963449f
nomi <nomibarbotaud26@gmail.com>
2025-05-16 08:53:53 +0200

    header backoffice correction des noms

--- phpTemplates/headerBackOffice.php
+++ phpTemplates/headerBackOffice.php
@@ -1,18 +1,18 @@
 <?php
 ?>
-<header class="BackOffice">
+<header class="back-office">
     <div class="logo">
         <img src="../html/images/logos/logoWhite.png" alt="Logo">
     </div>
 
     <h1>Bienvenue sur votre espace Pro !</h1>
 
-    <div class="headerRight">
+    <div class="header-droit">
         <a href="../html/proifile.html">
             <img src="../html/images/icons/person-circle-white.svg">
             <p>Toto Dupont</p>
         </a>
-    
+
         <a href="../html/deconnexion.html">
             <img src="../html/images/icons/box-arrow-right-white.svg">
         </a>
