58468d0f743ca978a5e545d9b7b17d790c09323c
capitainn <morgantsloan@gmail.com>
2025-05-22 16:29:11 +0200

    photo de profil active en tant que membre, et deconnexion membre ok

--- views/frontOffice/components/header.php
+++ views/frontOffice/components/header.php
@@ -1,23 +1,10 @@
 <script src="scripts/headerFront.js"></script>
 <header class="front-office">
     <div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-        </svg>
-        <?php
-            if(isset($_SESSION['id_utilisateur'])):
-        ?>
-        <img src="<?= $_SESSION['id_utilisateur']?>.png" alt="Photo de profil">
-        <?php
-            else:
-        ?>
         <img src="/images/logos/logoBlue.png" alt="logoBlue">
-        <?php
-            endif;
-        ?>
         <h1>PACT</h1>
     </div>
-<!-- Nav Cache car les pages ne sont pas encore créées
+<!-- Nav caché car les pages ne sont pas encore créées
     <nav>
         <ul>
             <li><a href="/index.php">Accueil</a></li>
@@ -26,10 +13,24 @@
         </ul>
     </nav>
 -->
-    <a href="/frontOffice/connexion/index.php">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-        </svg>
-    </a>
+    <?php if (isset($_SESSION['id_utilisateur'])): ?>
+        <!-- Utilisateur connecté : photo + bouton déconnexion -->
+        <div>
+            <img src="/images/profils/<?=$_SESSION['id_utilisateur']?>.jpg" alt="Photo de profil">
+            <a href="/scriptPHP/logout.php" title="Se déconnecter">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
+                    <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+                </svg>
+            </a>
+        </div>
+    <?php else: ?>
+        <!-- Utilisateur non connecté : bouton connexion -->
+        <a href="/frontOffice/connexion/index.php" title="Se connecter">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+            </svg>
+        </a>
+    <?php endif; ?>
 </header>
