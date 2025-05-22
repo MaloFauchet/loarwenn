ce69feb393c5d4cf7d82565df42fcb0876858bc3
Igor Galléa <igorgallea@gmail.com>
2025-05-22 09:04:23 +0200

    correction script peuplement

--- models/Professionnel.php
+++ models/Professionnel.php
@@ -1,9 +1,11 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../config/Database.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/../model/Model.php');
+
 
 class Professionnel extends Model{
 
-    public function getProfessionnelById($id) {
+    public function getProfessionnelParId($id) {
         $sql = "
             SELECT * FROM tripenazor.proffessionnel
             LEFT JOIN tripenazor.proffessionnel_prive on  tripenazor.proffessionnel.id_utilisateur
@@ -17,7 +19,21 @@ class Professionnel extends Model{
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function addProfessionnelPrive($nom, $prenom, $email, $telephone, $adresse, $complement, $codePostal, $ville, $denomination, $siren, $rib, $motDePasse) {
+        public function getProfessionnelParEmailMotDePasse($email, $mdp) {
+        $sql = "
+            SELECT * FROM tripenazor.proffessionnel
+            LEFT JOIN tripenazor.proffessionnel_prive on  tripenazor.proffessionnel.id_utilisateur
+            LEFT JOIN tripenazor.proffessionnel_public on  tripenazor.proffessionnel.id_utilisateur
+            INNER JOIN tripenazor.utilisateur on tripenazor.proffessionnel.id_utilisateur = tripenazor.utilisateur.id_utilisateur WHERE tripenazor.proffessionnel.id_utilisateur=$email, $mdp;
+        ";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function insertProfessionnelPrive($nom, $prenom, $email, $telephone, $adresse, $complement, $codePostal, $ville, $denomination, $siren, $rib, $motDePasse) {
         $sql = "
             SELECT inserer_utilisateur_et_professionnel()
         ";
@@ -28,10 +44,9 @@ class Professionnel extends Model{
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function addProfessionnelPublic($nom, $prenom, $email, $telephone, $adresse, $complement, $codePostal, $ville, $raisonSociale, $motDePasse) {
+    public function insertProfessionnelPublic($nom, $prenom, $email, $telephone, $adresse, $complement, $codePostal, $ville, $raisonSociale, $motDePasse) {
         $sql = "
             SELECT * FROM tripenazor.proffessionnel
-            INNER JOIN 
         ";
 
         $stmt = $this->conn->prepare($sql);
@@ -39,4 +54,6 @@ class Professionnel extends Model{
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+
 }
