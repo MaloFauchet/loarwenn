fd2069b9a6e6580ddeb695cbb965a762a889c871
capitainn <morgantsloan@gmail.com>
2025-06-12 17:58:31 +0200

    offre detaille responsive ameliorer

--- html/frontOffice/offreDetaille/index.php
+++ html/frontOffice/offreDetaille/index.php
@@ -6,6 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <title>PACT</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/styles/offreDetaille.css">
     <link rel="stylesheet" href="/styles/styles.css">
     <link rel="stylesheet" href="/styles/components/headerFrontOffice.css">
