687b823dd232ac9890cb36b6499950f538986227
Koro03 <benjamin.fauchet0309@gmail.com>
2025-06-12 10:24:42 +0200

    [FIX] refacto card + changement de tout les elements de la review

--- views/componentsGlobaux/cardHorizontal.php
+++ views/componentsGlobaux/cardHorizontal.php
@@ -1,5 +1,17 @@
 <a style="text-decoration:none;color:#011B43" href="<?="/frontOffice/offreDetaille/index.php?id=" . $valueOfOffre['id_offre'] ?>" class="a-card">
     <div class="card-horizontal">
+        <?php if ($valueOfOffre["Recommandé"]) { ?>
+            <div class="recommended-horizontal">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-patch-check-fill" viewBox="0 0 16 16">
+                        <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708" />
+                    </svg>
+                    <p>Recommandé</p>
+                </div>
+            </div>
+        <?php } ?>
+        
+        
         <div class="item-image">
             <img src="<?= $valueOfOffre["chemin"] ?$valueOfOffre["chemin"] : "/images/offres/missingImage.png" ?>" alt="<?php $valueOfOffre["titre_image"] ?>">
         </div>
@@ -18,41 +30,43 @@
                 </div>!-->
                 
             </div>
-            <div class="item-avis">
-                <p><?= $valueOfOffre["note_moyenne"]  ?></p>
-                <?= afficherEtoile($valueOfOffre["note_moyenne"])  ?>
-                <p><?= $valueOfOffre["nb_avis"]  ?></p>
-            </div>
-            <div class="item-description"> <?= $valueOfOffre["description"]  ?>
-            </div>
-            <div class="item-tag">
-                <?php foreach ($tabTag as $id_offre => $tags) { 
-                if ($id_offre === $valueOfOffre["id_offre"] ) { ?>
+            <div class="item-description"> <?= $valueOfOffre["description"]  ?> </div>
+            <div class="container-avis-tags">
+                <div class="item-avis">
+                        <p><?= $valueOfOffre["note_moyenne"]  ?></p>
+                        <?= afficherEtoile($valueOfOffre["note_moyenne"])  ?>
+                        <p><?= $valueOfOffre["nb_avis"]  ?></p>
+                    </div>
+                    </div>
+                    <div class="item-tag">
+                        <?php foreach ($tabTag as $id_offre => $tags) { 
+                        if ($id_offre === $valueOfOffre["id_offre"] ) { ?>
 
-                    <?php foreach ($tags as $tag => $value) { ?>
-                       <?php if($i<3){ ?>
+                            <?php foreach ($tags as $tag => $value) { ?>
+                            <?php if($i<3){ ?>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tags-fill" viewBox="0 0 16 16">
+                                        <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
+                                        <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043z"/>
+                                    </svg>
+                                    <p><?= $value ?></p>
+                                    
+                                </span>
+                            <?php $i++ ?>
+                            <?php } ?>
+                            <?php } ?>
+
+                    <?php } ?>
+                    <?php } ?>
+                    <?php if ($i === 3 ) { ?>
                         <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tags-fill" viewBox="0 0 16 16">
-                                <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
-                                <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043z"/>
-                            </svg>
-                            <p><?= $value ?></p>
-                            
+                            <p>...</p>
                         </span>
-                    <?php $i++ ?>
+                        <?php $i = 0?>
                     <?php } ?>
-                    <?php } ?>
-
-               <?php } ?>
-               <?php } ?>
-               <?php if ($i === 3 ) { ?>
-                   <span>
-                       <p>...</p>
-                   </span>
-                   <?php $i = 0?>
-               <?php } ?>
-               <?php $i = 0?>
+                    <?php $i = 0?>
+                    </div>
+                </div>
             </div>
-        </div>
     </div>
 </a>
--- views/componentsGlobaux/cardVerticalCaroussel.php
+++ views/componentsGlobaux/cardVerticalCaroussel.php
@@ -1,6 +1,16 @@
 <div>
 <a style="text-decoration:none;color:#011B43" href="<?="/frontOffice/offreDetaille/index.php?id=" . $valueOfOffre['id_offre'] ?>" class="a-card">
     <div>
+        <?php if ($valueOfOffre["Recommandé"]) { ?>
+            <div class="recommended">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-patch-check-fill" viewBox="0 0 16 16">
+                        <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708" />
+                    </svg>
+                    <p>Recommandé</p>
+                </div>
+            </div> 
+        <?php } ?>
         <div class="item-image">
             <img src="<?= $valueOfOffre["chemin"] ?$valueOfOffre["chemin"] : "/images/offres/missingImage.png" ?>" alt="<?php $valueOfOffre["titre_image"] ?>">
             
