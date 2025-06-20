126efbb3e87e7e95c0578dd14bb7feac8290df84
capitainn <morgantsloan@gmail.com>
2025-06-20 09:13:37 +0200

    correctif de la page d'accueil css

--- views/componentsGlobaux/cardHorizontal.php
+++ views/componentsGlobaux/cardHorizontal.php
@@ -11,46 +11,58 @@
                 </svg>
                 <p><?= $valueOfOffre["numero_adresse"] . ' ' . $valueOfOffre["voie"] . ', ' . $valueOfOffre["nom_ville"] . ', ' . $valueOfOffre["code_postal"]?></p>
             </div>
-            <div class="item-avis">
-                <?php if ($valueOfOffre["note_avis"]) : ?>
-                    <p><?= $valueOfOffre["note_avis"]  ?></p>
-                    <?= afficherEtoile($valueOfOffre["note_avis"]) ?>
-                    <p>(<?= $valueOfOffre["nb_avis"]  ?>)</p>
-                <?php endif; ?>
-            </div>
-            <div class="item-description"> <?= $valueOfOffre["description"]  ?>
-            </div>
-            <div class="item-tag">
+            <div class="item-description">
                 <?php 
-                if($valueOfOffre['tags']) {
-                    $allTagsOffre = explode(',', $valueOfOffre['tags']);
-                    $i = 0;
-                    foreach ($allTagsOffre as $id_offre => $tag) {
-                        if ($i < 3) { 
-                        ?>
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-tags-fill" viewBox="0 0 16 16">
-                                    <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3" />
-                                    <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043z" />
-                                </svg>
-                                <p><?= $tag ?></p>
-                            </span>
-                        <?php 
-                            $i++;
-                        } 
-                        if ($i === 3) { 
-                            ?>
-                            <span>
-                                <p>...</p>
-                            </span>
-                            <?php 
-                            break;
-                        } 
+                    $description =  substr($valueOfOffre["description"], 0, 200);
+                    if (strlen($valueOfOffre["description"]) > 200) {
+                        $description .= '...';
                     }
-                }
+                    echo $description;
                 ?>
             </div>
+            <div class="container-avis-tags">
+                <div class="item-avis">
+                    <?php if ($valueOfOffre["note_avis"]) : ?>
+                        <p><?= $valueOfOffre["note_avis"]  ?></p>
+                        <?= afficherEtoile($valueOfOffre["note_avis"]) ?>
+                        <p>(<?= $valueOfOffre["nb_avis"]  ?>)</p>
+                    <?php else: ?>
+                        <p>Aucun avis</p>
+                    <?php endif;?>
+                </div>
+                <div class="item-tag">
+                    <?php 
+                    if($valueOfOffre['tags']) {
+                        $allTagsOffre = explode(',', $valueOfOffre['tags']);
+                        $i = 0;
+                        foreach ($allTagsOffre as $id_offre => $tag) {
+                            if ($i < 3) { 
+                            ?>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-tags-fill" viewBox="0 0 16 16">
+                                        <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3" />
+                                        <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043z" />
+                                    </svg>
+                                    <p><?= $tag ?></p>
+                                </span>
+                            <?php 
+                                $i++;
+                            } 
+                            if ($i === 3) { 
+                                ?>
+                                <span>
+                                    <p>...</p>
+                                </span>
+                                <?php 
+                                break;
+                            } 
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
         </div>
+            
     </div>
 </a>
